fe43b6d201f0531c9806bb8975fb0e44a1895e64
zaki4Y <zaki44ygmail.com>
2024-03-10 20:49:00 +0000

    change title name to my portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 <Routes>
 
   <Route path="/" element={<HomePage />} />
-  <Route path="/about" element={<About />} />
+ 
 
   
 
--- src/pages/Home/components/portfolio.jsx
+++ src/pages/Home/components/portfolio.jsx
@@ -49,6 +49,9 @@ export const Portfolio = () => {
                   className="rounded-md duration-200 hover:scale-105"
                 />
                 <div className="flex items-center justify-center">
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  Demo
+                </button>
                   <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
                     Code
                   </button>
